b45a74d3aa6eabb532b00de8ddea8e26ef909175
Canmi <9997200@qq.com>
2025-09-25 11:46:33 +0800

    feat: add new client component

---
+++ src/client.tsx
@@ -0,0 +1,13 @@
+/* src/client.tsx */
+
+function App() {
+  return (
+    <main className="min-h-[100dvh] flex flex-col">
+      <div className="flex-1 flex justify-center items-center p-4">
+        //component here
+      </div>
+    </main>
+  );
+}
+
+export default App;
