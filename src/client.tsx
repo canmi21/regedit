41a90ccf19e5d497f330c4d54155d1931e87b407
Canmi <9997200@qq.com>
2025-09-25 15:15:05 +0800

    chore: remove unused imports and InstanceProvider route

--- src/client.tsx
+++ src/client.tsx
@@ -1,23 +1,14 @@
 /* src/client.tsx */
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { InstanceProvider } from "./contexts/instance-context";
-import HomePage from "./pages/home-page";
-import EditorPage from "./pages/editor-page";
 
 function App() {
   return (
-    <InstanceProvider>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route
-            path="/instance/:instanceId/explore/*"
-            element={<EditorPage />}
-          />
         </Routes>
       </Router>
-    </InstanceProvider>
   );
 }
 
